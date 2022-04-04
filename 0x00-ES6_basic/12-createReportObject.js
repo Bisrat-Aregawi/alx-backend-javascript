cea6c6444b32d78ce45b386edf79ea08824a443e
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-04-04 18:57:19 +0300

    Make a function to make a report object
    For task 12 of Solo-Project: - 0x00-ES6_basic

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,12 @@
+export default function createReportObject (employeesList) {
+  const report = {
+    allEmployees: {},
+    getNumberOfDepartments (employeesList) {
+      return Object.keys(employeesList).length;
+    }
+  };
+  for (const prop in employeesList) {
+    report.allEmployees[prop] = employeesList[prop];
+  }
+  return report;
+}
