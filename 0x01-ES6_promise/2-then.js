3bdc6abc8f78d39fb0433d2b44fadacf57483959
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-04-05 22:27:59 +0300

    Make promise handle function for both resolve and reject
    For task 2 of Solo-Project: - 0x01-ES6_promise

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      // eslint-disable-next-line no-console
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => new Error());
+}
