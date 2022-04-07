78a32533e77a4df4d1e15533e001019f41189a47
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-04-07 19:22:04 +0300

    Implement static method to eliminate duplicate code
    For task 2 of Solo-Project: - 0x02-ES6_classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,18 +1,18 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name === 'string') {
-      this._name = name;
-    } else {
-      throw TypeError('Name must be a string');
-    }
-    if (typeof length === 'number') {
-      this._length = length;
-    } else {
-      throw TypeError('Length must be a number');
-    }
+    HolbertonCourse.checkType(name, 'string', 'Name must be a string');
+    this._name = name;
+
+    HolbertonCourse.checkType(length, 'number', 'Length must be a number');
+    this._length = length;
+
     if (Array.isArray(students)) {
-      for (const elem in students) {
-        if (typeof elem !== 'string') throw TypeError('Student element not a string');
+      for (const elem of students) {
+        HolbertonCourse.checkType(
+          elem,
+          'string',
+          'Student element not a string',
+        );
       }
       this._students = students;
     } else {
@@ -35,29 +35,37 @@ export default class HolbertonCourse {
 
   // Settor methods
   set name(newName) {
-    if (typeof newName === 'string') {
-      this._name = newName;
-    } else {
-      throw TypeError('Name must be a string');
-    }
+    HolbertonCourse.checkType(newName, 'string', 'Name must be a string');
+    this._name = newName;
   }
 
   set length(newLength) {
-    if (typeof newLength === 'number') {
-      this._length = newLength;
-    } else {
-      throw TypeError('Length must be a number');
-    }
+    HolbertonCourse.checkType(newLength, 'number', 'Length must be a number');
+    this._length = newLength;
   }
 
   set students(newStudents) {
     if (Array.isArray(newStudents)) {
-      for (const elem in newStudents) {
-        if (typeof elem !== 'string') throw TypeError('Student element not a string');
+      for (const elem of newStudents) {
+        HolbertonCourse.checkType(
+          elem,
+          'string',
+          'Student element not a string',
+        );
       }
       this._students = newStudents;
     } else {
       throw TypeError('Students must be an array');
     }
+  }
+
+  // Static utility method
+  static checkType(arg, type, errMsg) {
+    /*
+      * Throw error if `arg` is not of type `type`
+      */
+    // eslint-disable-next-line valid-typeof
+    if (typeof arg !== type) throw TypeError(errMsg);
+    return true;
   }
 }
