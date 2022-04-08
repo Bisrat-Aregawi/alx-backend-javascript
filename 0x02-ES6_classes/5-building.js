44f44f8060157fbffe13bbcbd42a620d0b084c21
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-04-08 16:44:24 +0300

    Implement Building abstract class.
    All subclasses of Building must implement `evacuationWarningMessage`
    method.
    For task 5 of Solo-Project: - 0x02-ES6_classes

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,28 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor === Building) {
+      Building.checkType(sqft, Number, 'sqrft not a number');
+      this._sqft = sqft;
+    } else if (this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  // Getter methods
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Static method
+  static checkType(arg, type, errMsg) {
+    /**
+      * Throw error if `arg` is not of type `type`
+      * @param {string} arg variable to check type of
+      * @param {string} type string representing type required
+      * @param {string} errMsg message to show if arg is not required type
+      */
+    // eslint-disable-next-line valid-typeof
+    if (arg.constructor !== type) throw TypeError(errMsg);
+    return true;
+  }
+}
