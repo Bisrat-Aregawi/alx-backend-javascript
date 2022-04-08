d1789cdc90a3724ccb1d6a65acb0248d67c83231
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-04-08 17:12:04 +0300

    Implement class SkyHighBuilding
    Override `evacuationWarningMessage` and return a string
    
    For task 6 of Solo-Project: - 0x02-ES6_classes

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    Building.checkType(floors, Number, 'floors is not a number');
+    this._floors = floors;
+  }
+
+  // Getter methods
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
