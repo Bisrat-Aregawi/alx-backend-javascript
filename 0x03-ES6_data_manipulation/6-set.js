2ab45005ea0c1e01d552bfa71a476e6d2024a209
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-04-11 23:03:37 +0300

    Return set constructed from an array
    For task 6 of Solo-Project: - 0x03-ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+/**
+  * setFromArray - Function creates a set from passed array object
+  * @param {Array} arr - Array with any element types
+  * @returns {Set} A set from given array
+  */
+export default function setFromArray(arr) {
+  return new Set(arr);
+}
