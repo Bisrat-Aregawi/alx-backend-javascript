c135c01557d0a6c0327a1b38e451062b094ffe65
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-04-12 12:39:12 +0300

    Make WeakMap that tracks number of times of a function call
    Call to function should throw error for queries >= 5
    
    For task 11 of Solo-Project: - 0x03-ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,21 @@
+const weakMap = new WeakMap();
+
+/**
+  * queryAPI - Function is dummy API querying function
+  * @param {object} endpoint - An endpoint object
+  */
+export default function queryAPI(endpoint) {
+  if (weakMap.has(endpoint)) {
+    if (weakMap.get(endpoint) < 5) {
+      weakMap.set(
+        endpoint,
+        weakMap.get(endpoint) + 1,
+      );
+      return;
+    }
+    throw Error('Endpoint load is high');
+  }
+  weakMap.set(endpoint, 1);
+}
+
+module.exports = { queryAPI, weakMap };
