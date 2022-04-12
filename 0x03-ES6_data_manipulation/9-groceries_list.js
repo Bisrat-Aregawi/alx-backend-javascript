8f9c0b15f2ba37ff1d2867313babdc7270fe4134
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-04-12 11:04:47 +0300

    Return custom groceries map object
    For task 9 of Solo-Project: - 0x03-ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+/**
+  * groceriesList - Function creates a map of groceries
+  * @returns {Map}
+  */
+export default function groceriesList() {
+  return new Map([
+    ['Apple', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
