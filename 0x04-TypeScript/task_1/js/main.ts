5633138934d26f476f320f6af721b2e97ef7ebc8
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-04-14 12:25:07 +0300

    Create `StudentClass` class with constructor and class interfaces
    For task 4 of Solo-Project: - `0x04-TypeScript`

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -21,6 +21,33 @@ printTeacher = (firstName, lastName): string => {
 	return [firstName[0] + '.', lastName].join(' ');
 }
 
+interface StudentClassConstructor {
+	new (firstName: string, lastName: string): StudentClassInterface
+}
+
+interface StudentClassInterface {
+	workOnHomework(): string;
+	displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface{
+	firstName: string;
+	lastName: string;
+
+	constructor(firstName: string, lastName: string) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	workOnHomework(): string {
+		return 'Currently working';
+	}
+
+	displayName(): string {
+		return this.firstName;
+	}
+}
+
 const teacher3: Teacher = {
 	firstName: 'John',
 	fullTimeEmployee: false,
