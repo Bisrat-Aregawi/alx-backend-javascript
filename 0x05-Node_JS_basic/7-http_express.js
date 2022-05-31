f671e682365a68f89715dbb73a2fc01e02adf0b0
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-05-31 19:37:36 +0300

    Use express to respond to `/` and `/students` route
    For task 7 of Solo-Project: - `0x05-Node_JS_basic`

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,22 @@
+/**
+ * @module 7-http_express
+ */
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2])
+    .then((data) => {
+      res.send(data.join('\n'));
+    });
+});
+
+app.listen(port);
+
+module.exports = app;
