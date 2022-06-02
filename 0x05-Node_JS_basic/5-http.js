4460217b4d9b040491f1d528517a75bd68635857
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-06-02 14:01:08 +0300

    Fix checker error for task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,27 +2,35 @@
  * @module 5-http
  */
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const { exec } = require('child_process');
 
-// Define serving hostname
+// define serving hostname
 const hostname = '127.0.0.1';
-// Define serving port
+// define serving port
 const port = 1245;
 
-// Create a server
+// create a server
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/students') {
-    countStudents(process.argv[2])
-      .then((data) => {
-        const summary = `This is the list of our students\n${data.join('\n')}`;
-        res.setHeader('Content-Length', summary.length);
-        res.end(`This is the list of our students\n${data.join('\n')}`);
-      });
+    const cmd = 'const countStudents = require(\'./3-read_file_async\');'
+      + `countStudents('${process.argv[2]}')`
+      + '.catch((err) => console.error(err.message))';
+    exec(`echo "${cmd}" | ${process.execPath}`, (err, stdout, stderr) => {
+      if (stdout) {
+        const content = stdout.toString().slice(0, -1);
+        res.end(`This is the list of our students\n${content}`);
+      } else if (stderr) {
+        res.statusCode = 404;
+        const content = stderr.toString().slice(0, -1);
+        res.end(`This is the list of our students\n${content}`);
+      } else if (err) {
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      }
+    });
   } else {
     const resText = 'Hello Holberton School!';
-    res.setHeader('Content-Length', resText.length);
     res.end(resText);
   }
 });
